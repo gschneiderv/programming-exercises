f7584f668406306f45261063118b7aef93e2a54b
Gabriela <gschneiderv@gmail.com>
2025-05-14 14:59:37 +0200

    adding folders and create_table exercise

---
+++ Learning_SQL/functions.py
@@ -0,0 +1,3 @@
+USE sakila; #it's recomended to start with the name of the database to avoid mistakes. 
+SELECT NOW() # returns the current date and time ex: 2025-05-09 20:49:16
+
---
+++ Learning_SQL/table_creation.py
@@ -0,0 +1,27 @@
+
+#I will defineda table to hold information about a person
+
+# STEP 1: 'Design'
+#discuss: types of info that describes a person: Name, Eye Color, Birth date, Address, Favorite Foods
+
+#STEP 2: 'Refinement'
+#objetive: NORMALIZATION to ensure that there are no duplicates(other than FK) or compound coloumns in the database design
+#			define primary key(PK): person_id   ---> it guarantees uniqueness and will be the foreign key(FK) in the favourite_table
+
+#STEP 3: 'Building SQL Schema Statements'
+
+CREATE TABLE person
+(person_id SMALLINT UNSIGNED,
+ fname VARCHAR(20),
+ lname VARCHAR(20),
+ eye_color ENUM('BR','BL','GR'),
+ birth_date DATE,
+ street VARCHAR(20),
+ city VARCHAR(20),
+ state VARCHAR(20),
+ country VARCHAR(20),
+ postal_code VARCHAR(20),
+ CONSTRAINT pk_person PRIMARY KEY (person_id)
+);
+
+
