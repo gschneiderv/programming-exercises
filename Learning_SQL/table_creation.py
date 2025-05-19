cda785badef671cb2e14268301e8d7122078d07c
Gabriela <gschneiderv@gmail.com>
2025-05-19 15:14:31 +0200

    update, delete functions and  common errors of FK,PK, date format

--- Learning_SQL/table_creation.py
+++ Learning_SQL/table_creation.py
@@ -66,5 +66,68 @@ FROM person;
 WHERE person_id = 1;
  #it also coul be WHERE lname ='Turner';
 
- # imp bullets:
  
+ # NOTE: birth_date added is a string, al long as I match the required format, MySQL will convert the string to a date. 
+
+#'William Turner has 3 favorite foods' so:
+
+INSERT INTO favorite_food (person_id, food)
+VALUES (1, 'pizza'),
+	   (1, 'cookies'),
+	   (1, 'nachos');
+
+ #to check, using ORDER BY clause to tell the server how to sort the data returned by the query:
+SELECT	person_id ,
+		food 
+FROM favorite_food ff  
+WHERE person_id = 1
+ORDER BY food;
+
+#Add othe person:
+INSERT INTO person
+	(person_id, fname, lname, eye_color, birth_date, street, city, state, country, postal_code)
+VALUES (null, 'Susan', 'Smith', 'BL', '1975-11-02', '23 Mapple St.', 'Arlinton', 'VA', 'USA', '20220');
+
+#To see the data just added:
+SELECT person_id,
+	   fname,
+	   lname,
+	   eye_color,
+	   birth_date,
+	   street,
+	   city,
+	   state,
+	   country,
+	   postal_code
+FROM person;
+
+#Updating data for person_id =1 ('William'), using upsTE command
+
+UPDATE person 
+SET street = '1225 Tremont St.',
+	city = 'Boston',
+	state = 'MA',
+	country = 'USA', 
+	postal_code  = '02138'
+WHERE person_id = 1;
+
+# NOTE:  I could update more than one row, using: WHERE person_id < 10
+
+#Deleting data (ex: person #2)
+
+DELETE FROM person 
+WHERE person_id = 2;
+
+#NOTE: more than 1 row could be deteled using WHERE clause, and all rows could be deleted if the WHERE clause is omitted.
+
+#COMMON MISTAKES:
+# Nonunique PK : this error could appear if a write a number for the 'person_id' column that is repeated.
+# Nonexisting FK: you can't insert a number of person_id in favorite_table that doesn't exist in person table. 
+# Column Value Violations: if a column is restricted to some values, you can't insert a differente that is not an option (ex: color_eyes)
+# Invalid Date conversions: you can't write a string (a date) which is not in the format expected, ex: in birth_date will not match 'DEC-21-1980'
+# But, you can use str_to_date function to specify which format string to use:
+UPDATE person
+SET birth_date = str_to_date('DEC-21-1980', '%b-%d-%Y')
+WHERE person_id =1; 
+
+
