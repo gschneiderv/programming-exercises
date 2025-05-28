b63c75604a41ab0bf9c6203591a0af91d984981c
Gabriela <gschneiderv@gmail.com>
2025-05-28 11:53:24 +0200

    Chapter 3 finished

--- Learning_SQL/CH3_Query_Primer.sql
+++ Learning_SQL/CH3_Query_Primer.sql
@@ -133,3 +133,33 @@ SELECT
 	last_name 
 FROM actor a 
 WHERE last_name ='WILLIAMS' or last_name ='DAVIS';
+
+/*Exercise 3-3
+write a query against the rental table that returns the IDs of the customers who rented a film on July 5, 2005
+(use the r.rental_date column, and you can use the date() function to ignore the time component)
+Include a single row for each distinct customer ID
+*/
+SELECT DISTINCT customer_id 
+FROM rental r
+WHERE date(r.rental_date) = '2005-07-05'
+ORDER BY 1 ;
+
+/*Exercise 3-4
+Fill the blanks for the multitable query to achieve the following results:
+SELECT 
+	c.email,
+	r.return_date
+FROM customer c 
+INNER JOIN rental <1> 
+ON c.customer_id  = <2>
+WHERE date(r.rental_date) = '2005-06-14'
+ORDER BY <3> <4> ;
+*/
+SELECT 
+	c.email,
+	r.return_date 
+FROM customer c 
+INNER JOIN rental r 
+ON c.customer_id  = r.customer_id 
+WHERE date(r.rental_date) = '2005-06-14'
+ORDER BY 2 DESC;
