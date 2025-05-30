32c53cd53b28d7e0b2ee87bf2cba03cf1d7ad0b1
Gabriela <gschneiderv@gmail.com>
2025-05-30 16:48:42 +0200

    Using 2 tables twice

--- Learning_SQL/CH5_Querying_Multiple_Tables.sql
+++ Learning_SQL/CH5_Querying_Multiple_Tables.sql
@@ -79,3 +79,19 @@ FROM film f
 	ON fa.actor_id = a.actor_id 
 WHERE ((a.first_name = 'CATE' AND a.last_name = 'MCQUEEN')
 	OR (a.first_name = 'CUBA' AND a.last_name = 'BIRCH'));
+
+/*
+If I want both actors in the same films, I have to use the tables film_actor and actors twice:
+*/
+SELECT f.title
+FROM film f 
+	INNER JOIN film_actor fa1 
+	ON f.film_id = fa1.film_id
+	INNER JOIN actor a1 
+	ON fa1.actor_id = a1.actor_id 
+	INNER JOIN film_actor fa2 
+	ON f.film_id = fa2.film_id
+	INNER JOIN actor a2 
+	ON fa2.actor_id = a2.actor_id
+WHERE ((a1.first_name = 'CATE' AND a1.last_name = 'MCQUEEN')
+	AND (a2.first_name = 'CUBA' AND a2.last_name = 'BIRCH'));
