fbb5bd8e206e24e6f8a47ee68e25c61c0414409a
Gabriela <gschneiderv@gmail.com>
2025-05-30 16:43:31 +0200

    Joining 3 o more tables

--- Learning_SQL/CH5_Querying_Multiple_Tables.sql
+++ Learning_SQL/CH5_Querying_Multiple_Tables.sql
@@ -49,3 +49,33 @@ FROM customer c                     /*if I switch the tables in the joins condit
 	INNER JOIN city c2 
 	ON a.city_id = c2.city_id 
 ;	
+
+/*
+Using Subqueries as Tables (instead of joining several tables simultaneously)
+The following query joins the customer table to a subquery nagainst the address and city tables:
+*/
+SELECT 
+	c.first_name,
+	c.last_name ,
+	ad.address,
+	ad.city
+FROM customer c  
+	INNER JOIN (SELECT a.address, a.address_id,  c2.city
+				FROM address a  
+					INNER JOIN city c2 
+					ON a.city_id = c2.city_id 
+				WHERE a.district = 'California')AS ad
+	ON c.address_id = ad.address_id;
+
+/*
+Using the Same Table Twice
+Ex: Find all of the films in which 2 specific actors appear:
+*/
+SELECT f.title
+FROM film f 
+	INNER JOIN film_actor fa 
+	ON f.film_id = fa.film_id
+	INNER JOIN actor a 
+	ON fa.actor_id = a.actor_id 
+WHERE ((a.first_name = 'CATE' AND a.last_name = 'MCQUEEN')
+	OR (a.first_name = 'CUBA' AND a.last_name = 'BIRCH'));
