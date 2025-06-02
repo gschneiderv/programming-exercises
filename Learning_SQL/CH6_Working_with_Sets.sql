f207cf52a65bb61b8a554d3c9094805f32d6a7a1
Gabriela <gschneiderv@gmail.com>
2025-06-02 16:51:36 +0200

    Chapter 6: Working with sets

---
+++ Learning_SQL/CH6_Working_with_Sets.sql
@@ -0,0 +1,36 @@
+USE sakila;
+
+/*
+The Union and Union all Operators
+it allows to combine multiple data sets
+difference: union--> sorts combined dataset and removes duplicates.
+rows(union all) = rows(datasetA) + rows (datasetB)
+*/
+SELECT 
+	'CUST' typ, #to show that comes from the customer table
+	c.first_name ,
+	c.last_name 
+FROM customer c 	
+UNION ALL (SELECT 
+				'ACTR' typ, #to show that comes from the actor table
+				a.first_name, 
+				a.last_name 
+			FROM actor a);
+
+
+/*Test You Knowledge
+
+Exercise 6-1
+If setA = {L M N O P}
+   setB = {P Q R S T}
+What sets are generated? by:
+
+*)A  UNION  B ={L M N O P Q R S T}
+*)A  UNION ALL B = {L M N O P P Q R S T}
+*)A INTERSECT B = {P} 
+*) A EXCEPT B ={L M N O}
+
+Exercise 6-2
+Write a compound query that finds the first and last names of all actors and customers 
+whose last name starts with L
+*/
