5bb2d9338df0adc3a3a27cee0ffa0a9cdb2776d4
Gabriela <gschneiderv@gmail.com>
2025-06-03 14:18:42 +0200

    Chapter 7: Working with string data

---
+++ Learning_SQL/CH7_Data_Generation_Manipulation_and_Convertion.sql
@@ -0,0 +1,9 @@
+USE sakila;
+
+/*
+Working with String Data
+
+CHAR: values up 255 characters. Holds fixed-length, blank-padded strings.
+VARCHAR: values up 65.535 characters. Holds variable-length strings.
+TEXT: up 4GB in data size. Holds very large variable-length strings
+
