5e3c8a7286dcab29cabafc13a6bcc2e93664fc15
Gabriela <gschneiderv@gmail.com>
2025-06-20 12:46:02 +0200

    Updating grouping concepts

--- Learning_SQL/CH8_Grouping_&_Aggregates.sql
+++ Learning_SQL/CH8_Grouping_&_Aggregates.sql
@@ -7,4 +7,14 @@ SELECT
 	customer_id,
 	COUNT(rental_id) 
 FROM rental r 
-GROUP BY customer_id; 
+GROUP BY customer_id 
+ORDER BY 2 DESC;
+
+/*
+Also can use COUNT(*), it means that count everything in the group.
+
+ The filter to use wit Goup by clause is 'HAVING'  and goes after Group by clause (not use WHERE clause, it will give you an error) 
+ 
+ 
+ */
+
