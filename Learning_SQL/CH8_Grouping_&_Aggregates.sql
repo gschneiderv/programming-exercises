8419138194df8d7a380f1454103f549a297206a5
Gabriela <gschneiderv@gmail.com>
2025-06-26 16:02:53 +0200

    Aggregate functions

--- Learning_SQL/CH8_Grouping_&_Aggregates.sql
+++ Learning_SQL/CH8_Grouping_&_Aggregates.sql
@@ -13,8 +13,71 @@ ORDER BY 2 DESC;
 /*
 Also can use COUNT(*), it means that count everything in the group.
 
- The filter to use wit Goup by clause is 'HAVING'  and goes after Group by clause (not use WHERE clause, it will give you an error) 
+ The filter to use witH Goup by clause is 'HAVING'  and goes after Group by clause (not use WHERE clause, it will give you an error) 
+*/
+ SELECT  
+ 		customer_id,
+		COUNT(*)
+ FROM rental r
+ GROUP BY 1
+ HAVING COUNT(*) >=40;
+
+/*
+Aggregate functions:
+
+max() --> returns the maximum within set, ignore nulls
+
+min() --> returns the minimum value within set, ignore nulls
+
+avg() --> returns the average value across a set, ignore nulls
+
+sum() --> returns the sum of the value across a set
+
+count() --> returns the number of values in a set, imp!! COUNT(*)--> not ignores the nulls, while COUNT(val) ignores nulls 
+
+*/
+
+SELECT 
+	MAX(amount),
+	MIN(amount),
+	AVG(amount),
+	SUM(amount),
+	COUNT(amount)
+FROM payment p ;
+
+/*
+if i want to apply to each customer: i have to use group by clause
+With the inclusion of this clause the server knows to group together rows having the same value in the customer_id column first and then
+apply the five aggregates functions
+ */	
+SELECT customer_id ,
+	MAX(amount),
+	MIN(amount),
+	AVG(amount),
+	SUM(amount),
+	COUNT(amount)
+FROM payment p 
+GROUP BY customer_id ;
+
+/* 
+ Counting DISTINCT values
  
- 
+*/
+SELECT 
+	COUNT(customer_id) AS num_rows,
+	COUNT(DISTINCT customer_id) AS num_customers
+FROM payment p ;
+
+/*
+Using expressions
+
+ Ex: Find the maximum number of days between when a film was rented and subsequently returned.
  */
 
+SELECT 	
+	MAX(DATEDIFF(return_date ,rental_date)) 
+FROM rental r; 
+
+/*
+ HOW NULLs are handled--> see def of fuctions above
+*/
